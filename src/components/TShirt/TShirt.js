b0d075fb5f5d3696d4891703e4b3554e4e6a6f1e
Fahmidur-arnob <arnobdev93@gmail.com>
2022-10-07 22:32:03 +0600

    Added style to tshirts and home and the add-to-cart button

--- src/components/TShirt/TShirt.js
+++ src/components/TShirt/TShirt.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './TShirt.css';
 
 const TShirt = ({tShirt}) => {
-    const {picture, name} = tShirt;
+    const {picture, name, price} = tShirt;
     return (
-        <div>
+        <div className='T-shirt'>
             <img src={picture}></img>
             <h1>Name: {name}</h1>
+            <h2>Price: {price}</h2>
+            <button>Add to Cart</button>
         </div>
     );
 };
