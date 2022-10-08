bcae37b99880b6f7783f6e83049567c2eb370dff
Fahmidur-arnob <arnobdev93@gmail.com>
2022-10-08 20:36:47 +0600

    adding functionality to the add to cart button and adding tShirts to the cart part

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
@@ -6,6 +6,16 @@ import './Home.css';
 
 const Home = () => {
     const tShirts = useLoaderData();
+
+    const [cart, setCart] = useState([])
+
+
+    const handleAddToCart = tShirt =>{
+        const newCart = [...cart, tShirt];//notun ekta cart hoye gelo.. mutation korar drkr porlo na .. new ekta array create kore fellam .. 
+
+        setCart(newCart);//state e set hoye gelo
+
+    }
     return (
         <div className='home-container'>
             <div className="tshirt-continer">
@@ -13,11 +23,12 @@ const Home = () => {
                     tShirts.map(tShirt => <TShirt
                         key={tShirt.id}
                         tShirt={tShirt}
+                        handleAddToCart = {handleAddToCart}
                     ></TShirt>)
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
