65401fc4c6f46e5bef1d5244c9de8de7e7334318
Fahmidur-arnob <arnobdev93@gmail.com>
2022-10-09 11:32:33 +0600

    Added conditional rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import TShirt from '../TShirt/TShirt';
+// import TShirt from '../TShirt/TShirt';
 import './Cart.css';
 
 const Cart = ({cart, removeItem}) => {
+
+    let message;
+    if(cart.length === 0){
+        message = <p>You haven't bought anything till now.</p>
+    }
+    else{
+        message = <p>Thanks for buying from our store.</p>
+    }
+
     return (
         <div>
             <h2>Order Summary</h2>
@@ -15,8 +24,21 @@ const Cart = ({cart, removeItem}) => {
                         <button onClick={() => removeItem(tShirt)}   >Remove Item</button>
                     </p>)
             }
+            {
+                message
+            }
         </div>
     );
 };
 
 export default Cart;
+
+
+/*
+Conditional Rendering:
+
+1. Use element in a variable and then use if-else statements to set a value;
+
+2. Ternary operator er maddhome kora jabe.
+
+*/
